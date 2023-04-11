7953981d2cf7737c4b18dc911e27f318d17a8bb6
nikhilt <nikhil.t1226@gmail.com>
2023-04-11 14:56:17 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "${var.env}-docdb"
   engine                  = var.engine
+  engine_version          = var.engine_version
   master_username         = data.aws_ssm_parameter.user.value
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = true
+  skip_final_snapshot     = var.skip_final_snapshot
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
   kms_key_id              = data.aws_kms_key.key.arn
   storage_encrypted       = true
