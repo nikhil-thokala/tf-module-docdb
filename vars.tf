22b12cc10b510959c77381c1e45fc46dc0527153
nikhilt <nikhil.t1226@gmail.com>
2023-04-12 16:47:56 +0100

    \ |"?

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "engine" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
-variable "snapshot_identifier" {}
+variable "final_snapshot_identifier" {}
 variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
